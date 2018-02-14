145922c3a9f971963364d7004824cdc9c5e451b0
praveengits <praveenhears@gmail.com>
2018-02-14 13:54:42 -0600

    socket creation

--- server/server.js
+++ server/server.js
@@ -1,17 +1,35 @@
 const express = require('express');
-
+const socketIO = require('socket.io');
+const http = require('http');
 const path = require('path');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
+
+io.on('connection',(socket) => {
+    console.log('New user connected!');
+    
+});
+
+io.on('connection',(socket) => {
+    console.log('New user connected!');
+    
+    socket.on('disconnect', (socket)=> {
+        console.log('Disconnected from the user');                
+    });
+});
+
 
 // app.get('/', (req, res) => {
 //     res.render('index.html');
 // });
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`App listening on port ${port}!`);
 });
