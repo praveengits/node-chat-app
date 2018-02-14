cc0b5752c061e75a5ab3f23b24f6e720b6f77d05
praveengits <praveenhears@gmail.com>
2018-02-14 13:01:31 -0600

    init commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+// app.get('/', (req, res) => {
+//     res.render('index.html');
+// });
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}!`);
+});
